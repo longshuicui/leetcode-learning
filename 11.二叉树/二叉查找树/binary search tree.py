bc0aa9d098543bda75148c0c35170a37d1a300fa
longshuicui <longshuicui@qq.com>
2021-02-13 11:14:24 +0800

    递归实现二叉搜索树

--- 11.二叉树/二叉查找树/binary search tree.py
+++ 11.二叉树/二叉查找树/binary search tree.py
@@ -10,6 +10,7 @@
 二叉查找树是有序的，中序遍历就是排序号的数组
 """
 
+
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
@@ -19,15 +20,15 @@ class TreeNode:
 
 class BinarySearchTree:
     def __init__(self, root=None):
-        self.root=root
+        self.root = root
 
     def findMin(self):
         """左子节点小于等于父节点"""
         if self.root is None:
             return None
-        current=self.root
+        current = self.root
         while current.left:
-            current=current.left
+            current = current.left
         return current
 
     def findMax(self):
@@ -40,49 +41,106 @@ class BinarySearchTree:
         return current
 
     def insert(self, val):
-        node=TreeNode(val)
+        node = TreeNode(val)
         if self.root is None:
-            self.root=node
+            self.root = node
         else:
-            current=self.root
+            current = self.root
             while True:
-                if val<current.val:
+                if val < current.val:
                     if current.left is None:
-                        current.left=node
+                        current.left = node
                         return
-                    current=current.left
+                    current = current.left
                 else:
                     if current.right is None:
-                        current.right=node
+                        current.right = node
                         return
-                    current=current.right
+                    current = current.right
 
     def search(self, val):
-        current=self.root
+        current = self.root
         while current:
-            if current.val==val:
+            if current.val == val:
                 return current
-            current=current.left if val<current.val else current.right
+            current = current.left if val < current.val else current.right
         return current
 
-def inOrder(root):
-    """中序遍历"""
-    if not root:
-        return
-    inOrder(root.left)
-    print(root.val)
-    inOrder(root.right)
+
+class BST:
+    def insert(self, root, val):
+        """二叉搜索树插入操作"""
+        if root is None:
+            root = TreeNode(val)
+        elif val < root.val:
+            root.left = self.insert(root.left, val)
+        elif val > root.val:
+            root.right = self.insert(root.right, val)
+        return root
+
+    def find(self, root, val):
+        """二叉搜索树查询操作"""
+        if root is None:
+            return None
+        if root.val == val:
+            return root
+        elif val < root.val:
+            return self.find(root.left, val)
+        else:
+            return self.find(root.right, val)
+
+    def findMin(self, root):
+        """二叉搜索树返回最小值"""
+        if root.left:
+            return self.findMin(root.left)
+        else:
+            return root
+
+    def findMax(self, root):
+        """二叉搜素树返回最大值"""
+        if root.right:
+            return self.findMax(root.right)
+        else:
+            return root
+
+    def remove(self, root, val):
+        """二叉搜索树删除节点"""
+        if root is None:
+            return
+        if val < root.val:
+            root.left = self.remove(root.left, val)
+        elif val > root.val:
+            root.right = self.remove(root.right, val)
+        else:
+            if root.left and root.right:
+                # 如果当前节点存在左右子树，需要用右子树的最小值代替当前节点的值，然后删除右子树的最小值，递归处理
+                temp = self.findMin(root.right)
+                root.val = temp.val
+                root.right = self.remove(root.right, temp.val)  # 删除右子树的最小值
+            elif root.left is None and root.right is None:
+                # 如果当前节点左右子树都为空，直接删除该节点即可
+                root = None
+            elif root.left is None:
+                root = root.right
+            elif root.right is None:
+                root = root.left
+        return root
+
+    def printTree(self, root):
+        """中序遍历"""
+        if root is None:
+            return
+        self.printTree(root.left)
+        print(root.val, end=" ")
+        self.printTree(root.right)
 
 
 
-bst=BinarySearchTree()
-nums=[7, 5, 9, 8, 15, 16, 18, 17]
-for num in nums:
-    bst.insert(num)
-
-print(bst.findMin().val)
-print(bst.findMax().val)
-
-inOrder(bst.root)
-
-
+root=None
+bst=BST()
+root=bst.insert(root,10)
+root=bst.insert(root,8)
+root=bst.insert(root,6)
+root=bst.insert(root,15)
+node=bst.findMin(root)
+print(node.__dict__)
