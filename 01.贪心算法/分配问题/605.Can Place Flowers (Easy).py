c334b84848ab7e3c74c34157358f263239e54b48
longshuicui <longshuicui@qq.com>
2021-02-15 17:09:33 +0800

    rename

--- 01.贪心算法/分配问题/605.Can Place Flowers (Easy).py
+++ 01.贪心算法/分配问题/605.Can Place Flowers (Easy).py
@@ -15,13 +15,22 @@ https://leetcode.com/problems/can-place-flowers/
 
 
 def canPlaceFlowers(flowerbed, n):
-    size=len(flowerbed)
-    hasPlaced=sum(flowerbed)
-    if (hasPlaced+n) > size/2:
+    count = 0
+    i = 0
+    while i < len(flowerbed):
+        if flowerbed[i] == 0 \
+                and (i == 0 or flowerbed[i - 1] == 0) \
+                and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
+            count += 1
+            flowerbed[i] = 1
+            i += 1
+        i += 1
+    if count < n:
         return False
+    return True
 
 
 flowerbed=[1,0,0,0,0,1]
-n=1
+n=2
 res=canPlaceFlowers(flowerbed, n)
 print(res)
