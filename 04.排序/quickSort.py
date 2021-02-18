944dc5c19c525ec04ecf10227248f0fa5b854e11
longshuicui <lscui@vova.com.hk>
2021-02-18 18:03:06 +0800

    first commit

---
+++ 04.排序/quickSort.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@author: longshuicui
+@date  :   2021/2/18
+@function:
+"""
+
+
+def quickSort(nums, low, high):
+    if low >= high:
+        return nums
+    i, j = low, high
+    pivot = nums[low]
+    while i < j:
+        while i < j and nums[j] >= pivot:  # 寻找基准右侧比基准小的数
+            j -= 1
+        nums[i] = nums[j]
+        while i < j and nums[i] <= pivot:  # 寻找基准左侧比基准大的数
+            i += 1
+        nums[j] = nums[i]
+        # nums[i],nums[j]=nums[j],nums[i]
+    nums[j] = pivot
+    quickSort(nums, low, i - 1)
+    quickSort(nums, i + 1, high)
+    return nums
+
+
+nums = [1, 3, 2, 1]
+nums = quickSort(nums, 0, len(nums) - 1)
+print(nums)
--- 04.排序/tenSortAlgorithm.py
+++ 04.排序/tenSortAlgorithm.py
@@ -169,12 +169,12 @@ def bucketSort(nums, bucketSize):
 
 
 if __name__ == '__main__':
-    nums = [6, 5, 1, 9, 4, 3, 2, 7, 8]
+    nums = [2,3,1,1]
     # nums=selectionSort(nums)
     # nums=bubbleSort(nums)
     # nums=insertionSort(nums)
-    # nums=quickSort(nums)
+    nums=quickSort(nums)
     # nums = mergeSort(nums)
     # nums=bucketSort(nums, bucketSize=2)
-    nums = heapSort(nums)
+    # nums = heapSort(nums)
     print(nums)
