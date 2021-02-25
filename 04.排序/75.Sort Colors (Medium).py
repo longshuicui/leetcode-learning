9ae65e6ab053ccb1c45057a9b145c0b7781c8e1b
longshuicui <longshuicui@qq.com>
2021-02-25 22:48:02 +0800

    first commit

--- 04.排序/75.Sort Colors (Medium).py
+++ 04.排序/75.Sort Colors (Medium).py
@@ -26,22 +26,21 @@ Input: nums = [1]
 Output: [1]
 """
 
+
 def sortColors(nums):
-    left, right, current=0,len(nums)-1,0
-    while current<=right:
-        if nums[current]==0:
-            nums[current], nums[left]=nums[left], nums[current]
-            current+=1
-            left+=1
-        elif nums[current]==1:
-            current+=1
-        elif nums[current]==2:
-            nums[right], nums[current]=nums[current], nums[right]
-            right-=1
+    left, right, current = 0, len(nums) - 1, 0
+    while current <= right:
+        if nums[current] == 0:
+            nums[current], nums[left] = nums[left], nums[current]
+            current += 1
+            left += 1
+        elif nums[current] == 1:
+            current += 1
+        elif nums[current] == 2:
+            nums[right], nums[current] = nums[current], nums[right]
+            right -= 1
 
 
-
-
-nums=[2,0,2,1,1,0]
+nums = [2, 0, 2, 1, 1, 0]
 sortColors(nums)
 print(nums)
