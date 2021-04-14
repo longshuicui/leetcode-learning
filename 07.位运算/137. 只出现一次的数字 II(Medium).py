561062b42193173ed99782ba539bf6e442636537
longshuicui <lscui@vova.com.hk>
2021-04-14 10:38:43 +0800

    first commit

---
+++ 07.位运算/137. 只出现一次的数字 II(Medium).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: longshuicui
+@date  :   2021/4/14
+@function:
+137. 只出现一次的数字 II (Medium)
+https://leetcode-cn.com/problems/single-number-ii/
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。
+示例：
+    输入: [2,2,3,2]
+    输出: 3。
+
+    输入: [0,1,0,1,0,1,99]
+    输出: 99
+"""
+
