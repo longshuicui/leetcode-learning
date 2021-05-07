d801944af0ef1f9f0d5cc16881eaa328dbe0c32a
longshuicui <lscui@vova.com.hk>
2021-05-07 09:39:03 +0800

    first commit

---
+++ 07.位运算/1486. 数组异或操作(Easy).py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+@author: longshuicui
+@date  :   2021/5/7
+@function:
+1486. 数组异或操作 (Easy)
+https://leetcode-cn.com/problems/xor-operation-in-an-array/
+给你两个整数，n 和 start 。
+数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
+请返回 nums 中所有元素按位异或（XOR）后得到的结果。
+示例 1：
+    输入：n = 5, start = 0
+    输出：8
+    解释：数组 nums 为 [0, 2, 4, 6, 8]，其中 (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8 。
+         "^" 为按位异或 XOR 运算符。
+示例 2：
+    输入：n = 4, start = 3
+    输出：8
+    解释：数组 nums 为 [3, 5, 7, 9]，其中 (3 ^ 5 ^ 7 ^ 9) = 8.
+示例 3：
+    输入：n = 1, start = 7
+    输出：7
+示例 4：
+    输入：n = 10, start = 5
+    输出：2
+
+"""
+
+def xorOperation(n, start):
+    """时间复杂度为O(n), 空间复杂度为O(1)"""
+    ans=0
+    for i in range(n):
+        ans^=start+2*i
+    return ans
+
+n=10
+start=5
+res=xorOperation(n, start)
+print(res)
--- 07.位运算/1720. 解码异或后的数组(Easy).py
+++ 07.位运算/1720. 解码异或后的数组(Easy).py
@@ -4,6 +4,7 @@
 @date  :   2021/5/6
 @function:
 1720. 解码异或后的数组 (Easy)
+https://leetcode-cn.com/problems/decode-xored-array/
 未知 整数数组 arr 由 n 个非负整数组成。
 经编码后变为长度为 n - 1 的另一个整数数组 encoded ，其中 encoded[i] = arr[i] XOR arr[i + 1] 。例如，arr = [1,0,2,1] 经编码后得到 encoded = [1,2,3] 。
 给你编码后的数组 encoded 和原数组 arr 的第一个元素 first（arr[0]）。
